6765a4f0e9ab20acdda124bb8cdf5ce7dec0a1ae
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-09-27 19:34:03 +0200

    [TASK] Added sitepackage repo

---
+++ Source/Extension/sitepackage/ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Site package',
+    'description' => '',
+    'category' => 'templates',
+    'author' => 'Hoja Mustaffa Abdul Latheef',
+    'author_email' => 'extensions@jweiland.net, extensions@jweiland.net',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '7.6.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-7.6.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
